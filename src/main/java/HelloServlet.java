6e1d59d6bbf04ac641aee54066bf77ced1c8997d
chenxiaolong <415318545@qq.com>
2018-01-13 16:28:11 +0800

    first commit

--- src/main/java/HelloServlet.java
+++ src/main/java/HelloServlet.java
@@ -14,10 +14,15 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
 
         PrintWriter out = null;
         try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("驱动初始化成功");
             out = response.getWriter();
             out.println("<h1>Hello Servlet!</h1>");
             out.println(new Date());
         } catch (IOException e) {
+            e.printStackTrace();
+
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
